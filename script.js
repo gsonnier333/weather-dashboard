cb4dfdb94088c7b2e6f60de12943608b3039dc7f
Greg Sonnier <gsonnier3@gmail.com>
2021-02-02 21:06:36 -0700

    Added five day forecast

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiKey = "&appid=d592dcfacf2862a2f02f038fed6d4e51";
 var onecallUrl = "https://api.openweathermap.org/data/2.5/onecall?";
+var loading = false; //use this boolean to keep the user from spamming the search button while their previous search loads
 
 $(document).ready(function(){
+    
+    
     
     function showWeather(apiObj){
         $("#todayTemp").text("Temperature: " + apiObj.current.temp + " F");
         $("#todayHum").text("Humidity: " + apiObj.current.humidity + "%");
         $("#todayWind").text("Wind Speed: " + apiObj.current.wind_speed + " MPH");
         $("#uvIndex").text("UV Index: " + apiObj.current.uvi);
+        let [month, date, year] = new Date().toLocaleDateString("en-US").split("/");
+        for(var i = 1; i < 6; i++){ //populate the 5-day forecast
+            var weather = apiObj.daily[i-1].weather[0].main;
+            if(weather === "Clear"){
+                weather = "☀";
+            }
+            else if(weather === "Clouds"){
+                weather = "☁"
+            }
+            else if(weather === "Rain"){
+                weather = "🌧"
+            }
+            else if(weather === "Drizzle"){
+                weather = "🌦"
+            }
+            else if(weather === "Thunderstorm"){
+                weather = "⛈"
+            }
+            else if(weather === "Snow"){
+                weather = "❄"
+            }
+            else{
+                weather = "❓" //if the weather is none of the above, probably atmosphere
+            }
+            $("#"+i).text(month + "/" + (parseInt(date) + parseInt(i)) + "/" + year + " " + weather + " Temp: " + apiObj.daily[i-1].temp.day + "F " + "Humid: " + apiObj.daily[i-1].humidity + "%"); //note: this will work poorly near the end of the month, consider a different approach in the future
+            
+        }
+        
+        loading = false; //let it know it's done loading
     }
     
     function forecast(city){
+        if(loading){return;} //tell it to stop if it's loading
+        loading = true;
         var day = new Date(Date.now()).toDateString(); //keep track of the current date to display
         $.ajax({
             url: queryUrl + city + apiKey, //call the api using the city name
